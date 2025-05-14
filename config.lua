937d4d270a8075c2a8d039032ced40ce94fc924d
Samuelmooregaming-games <samweidmanqwe@gmail.com>
2025-05-13 21:37:44 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,5 @@
 
 function love.conf(t)
-t.console = true
-t.window.setIcon( love.image.newImageData("Textures/RecallRushIcon.png") )
+t.window.setIcon(love.image.newImageData("Textures/RecallRushIcon.png") )
 t.window.setTitle('Recall Rush')
 end
